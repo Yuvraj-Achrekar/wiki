aceef6fabd18bab2eda3c43af56ba1d7507e9b42
Yuvraj-Achrekar <yuvraj.achrekar@gmail.com>
2024-04-01 17:56:42 +0530

    Django practice project

---
+++ encyclopedia/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'wiki'
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("wiki/<str:name>", views.entry, name="entry"),
+]
---
+++ encyclopedia/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def entry(request,name):
+    return render(request,"encyclopedia/entry.html",{
+        "entries": name
+    })
