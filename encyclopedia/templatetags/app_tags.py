75995a30f2e7d6cf36e5f99ebf39bf01b9b8b66b
Yuvraj-Achrekar <yuvraj.achrekar@gmail.com>
2024-04-06 15:13:03 +0530

    Random page completed

--- encyclopedia/templatetags/app_tags.py
+++ encyclopedia/templatetags/app_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 import random
 from .. import util
 
@@ -6,9 +8,5 @@ register = template.Library()
 
 @register.simple_tag
 def random_generator():
-    entries = util.list_entries()
-    if entries:
-        random_entry = random.choice(entries)
-        return random_entry
-    else:
-        return '' 
+    random_entry = random.choice(util.list_entries())
+    return random_entry
